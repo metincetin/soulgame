efee52401a3b86b3a5d3682041cd07349dcc7296
Metin Çetin <metinc309@gmail.com>
2021-05-22 03:29:04 +0300

    Added GameInstance

---
+++ scripts/auto_load/GameInstance.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var gold = 0
+var skills = []
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
