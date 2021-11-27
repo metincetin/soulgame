d041ae557e585752d3cd138a49aff0c2528a7dc8
Metin ÇETİN <metinc309@gmail.com>
2021-11-28 00:53:05 +0300

    Added knocback and knocback test scene

---
+++ scripts/Enemy.gd
@@ -0,0 +1,19 @@
+extends Character
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+export var knocback_resistance = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func knockback(power:Vector3):
+	external_forces += power / knocback_resistance
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
